bf0cbfb8208f1ab8f8c7e5ca3fc9da31bfd2d212
xy <wang.xy.chn@gmail.com>
2013-08-28 09:58:45 +0800

    add a helloworld component

---
+++ game-server/app/components/HelloWorld.js
@@ -0,0 +1,36 @@
+module.exports = function(app, opts) {
+  return new HelloWorld(app, opts);
+};
+
+
+var DEFAULT_INTERVAL = 3000;
+
+var HelloWorld = function(app, opts) {
+  this.app = app;
+  this.interval = opts.interval || DEFAULT_INTERVAL;
+  this.timerId = null;
+};
+
+HelloWorld.name = '__HelloWorld__';
+
+HelloWorld.prototype.start = function (cb) {
+  console.log('Hello World Start');
+  var self = this;
+  this.timerId = setInterval(function() {
+    console.log(self.getServerId() + "Hello World!");
+    }, this.interval);
+  process.nextTick(cb);
+}
+
+HelloWorld.prototype.afterStart = function (cb) {
+  console.log('Hello World afterStart');
+  process.nextTick(cb);
+}
+
+HelloWorld.prototype.stop = function (cb) {
+  cosole.log('Hello World stop');
+  clearInterval(this.timerId);
+  process.nextTick(cb);
+}
+
+
